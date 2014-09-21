55f86ddcfd20e971b5051fdb21fe0b5dd141ba05
ThomasLau <liuyongzhi0218@gmail.com>
2014-09-22 02:26:08 +0800

    more commit

--- src/listennerqr.js
+++ src/listennerqr.js
@@ -6,6 +6,25 @@ $(document).ready(function () {
 		height : 150
 	});
 	if(detail.status){
-		qrcode.makeCode(detail.url);
+		qrcode.makeCode(utf16to8(detail.url));
 	}
 });
+function utf16to8(str) {  
+	var out, i, len, c;  
+	out = "";  
+	len = str.length;  
+	for(i = 0; i < len; i++) {  
+	c = str.charCodeAt(i);  
+	if ((c >= 0x0001) && (c <= 0x007F)) {  
+		out += str.charAt(i);  
+	} else if (c > 0x07FF) {  
+		out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));  
+		out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));  
+		out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));  
+	} else {  
+		out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));  
+		out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));  
+	}  
+	}  
+	return out;  
+}  
